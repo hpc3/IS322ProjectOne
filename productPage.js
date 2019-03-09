18611b8b00f35169c28c97afd3684eaf5d2248a7
Peter's PC <phe3@njit.edu>
2019-03-08 19:06:49 -0500

    Working on Product Page

---
+++ productPage.js
@@ -0,0 +1,47 @@
+
+
+//To do
+// Filter :^(
+// fill mockDB with jazz
+//style cards
+
+(function () {
+
+
+
+    var mockDatabase =
+        [
+            {_id : '1', model: '*Joke About Camo*',color : 'look its a color', style : 'it’s a style can you believe it', wireless: false, price: '4', description: 'its a description', brand: 'Mr. Banana Grabber' , photo: 'media/headphone-image-11.jpg'},
+            {_id : '2', model: 'NightHawk 5000\'s', color : 'look its a color', style : 'it’s a style can you believe it', wireless: false, price: '4', description: 'its a description', brand: 'Mr. Banana Grabber' , photo: 'media/headphone-image-12.jpg'},
+            {_id : '3', model: 'MDRXMIII', color : 'Black', style : 'Around the Ear', wireless: true, price: '350', description: 'Awesome Noise Cancelling headphones', brand: 'Sony', photo: 'media/headphone-image-1.jpg'},
+            {_id : '4',  model: 'MDRXMIII',color : 'Gold', style : 'Around the Ear', wireless: true, price: '350', description: 'Awesome Noise Cancelling headphones', brand: 'Sony', photo: 'media/headphone-image-2.jpg'},
+            {_id : '5', model: 'Comfort Quiet 35', color : 'Black', style : 'Around the Ear', wireless: true, price: '350', description: 'Awesome Noise Cancelling headphones', brand: 'Bose', photo: 'media/headphone-image-3.jpg'},
+            {_id : '6', model: 'Comfort Quiet 35', color : 'Silver', style : 'Around the Ear', wireless: true, price: '350', description: 'Awesome Noise Cancelling headphones', brand: 'Bose', photo: 'media/headphone-image-4.jpg'},
+            {_id : '7',  model: 'Comfort Quiet 35',color : 'Blue', style : 'Around the Ear', wireless: true, price: '350', description: 'Awesome Noise Cancelling headphones', brand: 'Bose', photo: 'media/headphone-image-5.jpg'},
+            {_id : '8', model: 'Solo3', color : 'Black', style : 'Over the Ear', wireless: false, price:'300', description: 'Awesome Wireless headphones headphones', brand: 'Beats', photo: 'media/headphone-image-6.jpg'},
+            {_id : '9', model: 'Solo3', color : 'Matte Black', style : 'Over the Ear', wireless: false, price:'300', description: 'Awesome Wireless headphones headphones', brand: 'Beats', photo: 'media/headphone-image-7.jpg'},
+            {_id : '10', model: 'Solo3', color : 'Silver', style : 'Over the Ear', wireless: false, price:'300', description: 'Awesome Wireless headphones headphones', brand: 'Beats', photo: 'media/headphone-image-8.jpg'},
+            {_id : '11', model: 'Solo3', color : 'Gold', style : 'Over the Ear', wireless: false, price:'300', description: 'Awesome Wireless headphones headphones', brand: 'Beats', photo: 'media/headphone-image-9.jpg'},
+            {_id : '12', model: 'Solo3',color : 'Rose Gold', style : 'Over the Ear', wireless: false, price:'300', description: 'Awesome Wireless headphones headphones', brand: 'Beats', photo: 'media/headphone-image-10.jpg'}
+        ];
+
+    //Css '...'
+
+    function renderProducTitle(results){
+        var productPage = document.querySelector('#Products h3');
+                var filler = results.map(function(result){
+            return '<p>' + results.model +'</p>';
+        });
+
+
+    }
+
+
+    renderCards(mockDatabase);
+
+})();
+
+
+
+
+
